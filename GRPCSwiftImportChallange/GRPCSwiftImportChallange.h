c4e26619d254646b5b36daa4e714c3e0e7b09bfa
Kosuke Ohmura <little-parrot@hotmail.co.jp>
2020-02-03 16:10:18 +0900

    Add project

---
+++ GRPCSwiftImportChallange/GRPCSwiftImportChallange.h
@@ -0,0 +1,19 @@
+//
+//  GRPCSwiftImportChallange.h
+//  GRPCSwiftImportChallange
+//
+//  Created by Kosuke Omura on 2020/01/31.
+//  Copyright © 2020 kosukeohmura. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GRPCSwiftImportChallange.
+FOUNDATION_EXPORT double GRPCSwiftImportChallangeVersionNumber;
+
+//! Project version string for GRPCSwiftImportChallange.
+FOUNDATION_EXPORT const unsigned char GRPCSwiftImportChallangeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRPCSwiftImportChallange/PublicHeader.h>
+
+
---
+++ GRPCSwiftImportChallange/HelloWorld.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.swift
+//  GRPCSwiftImportChallange
+//
+//  Created by Kosuke Omura on 2020/01/31.
+//  Copyright © 2020 kosukeohmura. All rights reserved.
+//
+
+import Foundation
+import GRPC
+
+class HelloWorld {
+    init() {
+        print("Hello, World!")
+    }
+}
