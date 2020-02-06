a61292c48f08e38154449ab88f9da4e6b5122656
Kosuke Ohmura <little-parrot@hotmail.co.jp>
2020-02-06 12:23:27 +0900

    Add an iOS app that imports GRPCSwiftImportChallange.xcframework

---
+++ AppImportsXCFramework/AppImportsXCFramework/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AppImportsXCFramework
+//
+//  Created by Kosuke Omura on 2020/02/06.
+//  Copyright © 2020 kosukeohmura. All rights reserved.
+//
+
+import SwiftUI
+import GRPCSwiftImportChallange
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
